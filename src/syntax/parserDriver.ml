216bec509f8e92004c39cb7d41e22a8fdd40369a
Simon Krajewski <simon@haxe.org>
2017-02-24 18:03:20 +0100

    always use position of last shift for insertions

--- src/syntax/parserDriver.ml
+++ src/syntax/parserDriver.ml
@@ -374,11 +374,10 @@ and loop : 'a . (Config.t * TokenProvider.t) -> 'a State.t -> 'a result =
 		let fail () =
 			loop (config,tp) {state with checkpoint = I.resume state.checkpoint}
 		in
-		let p = tp.TokenProvider.lexbuf.pos in
 		begin match state.last_shift with
-			| ((BRCLOSE,_,_),trivia) when not (was_inserted trivia) && acceptable SEMICOLON p -> insert SEMICOLON true p
+			| ((BRCLOSE,_,p),trivia) when not (was_inserted trivia) && acceptable SEMICOLON p -> insert SEMICOLON true p
 			| _ when not config.recover -> fail()
-			| _ ->
+			| ((_,_,p),_) ->
 				if acceptable SEMICOLON p then insert SEMICOLON false p
 				else if acceptable PCLOSE p then insert PCLOSE false p
 				else if acceptable BRCLOSE p then insert BRCLOSE false p
