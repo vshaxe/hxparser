ff760a8c64a8331a7c3069b1775e4761ddb98db9
Gama11 <jensfischer95@gmail.com>
2017-02-28 19:02:14 +0100

    Restrict deployment to the master branch

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+if [[ $TRAVIS_BRANCH != 'master' ]]
+	exit
+fi
+
+openssl aes-256-cbc -K $encrypted_4ed650af1ebb_key -iv $encrypted_4ed650af1ebb_iv -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+cd ..
+ssh -T git@github.com
+git clone git@github.com:/vshaxe/haxe-hxparser.git
+cd haxe-hxparser
+cp ../hxparser/hxparserjs.js .
+git config user.name 'Travis CI'
+git config user.email '<>'
+git add -A
+git commit -m "Update hxparserjs.js"
+git push origin
